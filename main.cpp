625bc31911a00e2186da88ed76565dd643f8b9dc
Deflnam <burukov01@bk.ru>
2025-10-25 12:37:09 +0300

    Input one unsign

--- main.cpp
+++ main.cpp
@@ -2,6 +2,26 @@
 
 int main()
 {
-    
+    using u_t = unsigned;
+    u_t a=0;
+    std::cin>>a;
+
+
+    if(std::cin.eof())
+    {
+        std::cout << 0;
+        std::cout<<'\n';
+
+    }
+
+    else if (std::cin.fail())
+    {
+        std::cerr<< "Error\n";
+        return 1;
+
+    }
+
+
+
 
 }
