5f141628a0ab23da4192982531c58bb54e2325e1
Deflnam <burukov01@bk.ru>
2025-10-25 17:07:34 +0300

    Complete CLASS TASK

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <climits>
 
 bool isPyth(unsigned a, unsigned b, unsigned c);
+bool checkOverflow(unsigned a, unsigned b, unsigned c);
 
 
 
@@ -14,6 +16,12 @@ int main()
 
     while(std::cin>>a)
     {
+        if (checkOverflow(a, b, c))
+        {
+            std::cerr << "Error\n";
+            return 2;
+        }
+
         count += isPyth(a,b,c)? 1:0;
         c=b;
         b=a;
@@ -34,6 +42,7 @@ int main()
 
     }
 
+
 }
 
 bool isPyth(unsigned a, unsigned b, unsigned c )
@@ -42,4 +51,24 @@ bool isPyth(unsigned a, unsigned b, unsigned c )
     p = p || (b*b == (a*a + c*c));
     p = p || (c*c == (a*a + b*b));
     return p;
+}
+
+bool checkOverflow(unsigned a, unsigned b, unsigned c)
+{
+   
+    if (a > 1 && a > (std::numeric_limits<unsigned>::max() / a)) return true;
+    if (b > 1 && b > (std::numeric_limits<unsigned>::max() / b)) return true;
+    if (c > 1 && c > (std::numeric_limits<unsigned>::max() / c)) return true;
+    
+    unsigned a2 = a * a;
+    unsigned b2 = b * b;
+    unsigned c2 = c * c;
+    
+    
+    if (b2 > std::numeric_limits<unsigned>::max() - c2) return true;
+    unsigned ab_sum = a2 + b2;
+    if (ab_sum < a2 || ab_sum < b2) return true; 
+    if (ab_sum > std::numeric_limits<unsigned>::max() - c2) return true;
+    
+    return false;
 }
