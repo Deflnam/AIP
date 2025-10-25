0362b76e5c84c26b4bd50dfbbd4eb9a1abe2a38f
Deflnam <burukov01@bk.ru>
2025-10-25 13:21:51 +0300

    Complite task

--- main.cpp
+++ main.cpp
@@ -3,11 +3,14 @@
 bool isPyth(unsigned a, unsigned b, unsigned c);
 
 
+
 int main()
 {
     using u_t = unsigned;
     u_t a=0,b=0,c=0;
     size_t count = 0;
+    std::cin>>c>>b;
+    
 
     while(std::cin>>a)
     {
@@ -30,9 +33,6 @@ int main()
         return 1;
 
     }
-
-
-
 
 }
 
