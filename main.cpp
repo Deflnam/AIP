2930a60e64912cfb7dfa21320918464b3083a2c3
Deflnam <burukov01@bk.ru>
2025-10-25 12:50:17 +0300

    Sequence

--- main.cpp
+++ main.cpp
@@ -4,12 +4,17 @@ int main()
 {
     using u_t = unsigned;
     u_t a=0;
-    std::cin>>a;
+    size_t count = 0;
+
+    while(std::cin>>a)
+    {
+        count++;
+    }
 
 
     if(std::cin.eof())
     {
-        std::cout << 0;
+        std::cout << count;
         std::cout<<'\n';
 
     }
